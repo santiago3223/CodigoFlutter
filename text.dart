4a5ffe524f929c200a4819c9b606421c8704f62a
Santiago <gerencia@evolutionsoluciones.com>
2020-07-22 18:33:24 -0500

    teoria condicionales

--- text.dart
+++ text.dart
@@ -1,6 +1,24 @@
-import 'dart:io';
-
-void main(List<String> args) {
-  var name = stdin.readLineSync();
-  print(name);
+void main() {
+  int edad = 100;
+  String sexo = "Robot";
+  String tipo = "Staff";
+  if (tipo == "Staff") {
+    print("Puede ingresar");
+  } else if (sexo == "Masculino") {
+    if (edad >= 18 && edad <= 35) {
+      print("Puede ingresar");
+    } else {
+      print("No puede ingresar");
+    }
+  } else {
+    if (sexo == "Femenino") {
+      if (edad >= 16 && edad <= 35) {
+        print("Puede ingresar");
+      } else {
+        print("No puede ingresar");
+      }
+    } else {
+      print("No puede ingresar");
+    }
+  }
 }
