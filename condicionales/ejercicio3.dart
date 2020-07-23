621908d8fb2e1cf5eee1f948a1c6e2eff9279684
Santiago <gerencia@evolutionsoluciones.com>
2020-07-22 19:05:32 -0500

    ejercicio 2 y 3

---
+++ condicionales/ejercicio3.dart
@@ -0,0 +1,6 @@
+void main() {
+  int numero = 50;
+  if (numero > 0) {
+    print("El ${numero} es positivo");
+  }
+}
---
+++ condicionales/lab2.dart
@@ -0,0 +1,10 @@
+void main() {
+  double nota = 15;
+  if (nota < 10 && nota >= 0) {
+    print("Desaprobado");
+  } else if (nota >= 11 && nota <= 20) {
+    print("Aprobado");
+  } else {
+    print("Nota No Valida");
+  }
+}
