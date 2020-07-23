bcddc345dc1c518195f1ad5922d6caaab117b56b
Santiago <gerencia@evolutionsoluciones.com>
2020-07-22 19:27:15 -0500

    Ejercicio 4

--- condicionales/ejercicio4.dart
+++ condicionales/ejercicio4.dart
@@ -1,25 +1,25 @@
 void main(List<String> args) {
-  double sueldo = 51000;
-  int porcentaje;
+  double sueldo = 20000;
+  double porcentaje;
   String mostrar;
 
   if (sueldo <= 0) {
     mostrar = "Sueldo Inválido";
   } else if (sueldo < 10000) {
-    porcentaje = 3;
+    porcentaje = 0.03;
   } else if (sueldo < 20000) {
-    porcentaje = 7;
+    porcentaje = 0.07;
   } else if (sueldo < 30000) {
-    porcentaje = 9;
+    porcentaje = 0.09;
   } else if (sueldo < 40000) {
-    porcentaje = 13;
+    porcentaje = 0.13;
   } else if (sueldo < 50000) {
-    porcentaje = 16;
+    porcentaje = 0.16;
   } else {
-    porcentaje = 20;
+    porcentaje = 0.20;
   }
 
-  double sueldoneto = sueldo * (100 - porcentaje) / 100;
+  double sueldoneto = sueldo * (1 - porcentaje);
 
   mostrar =
       "El sueldo es ${sueldo} y el impuesto es el ${porcentaje}% sueldo Neto: ${sueldoneto}";
