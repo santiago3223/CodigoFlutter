6d3da41140f631ac810e91485ffda6143c0f5564
Santiago <gerencia@evolutionsoluciones.com>
2020-07-23 21:17:27 -0500

    Ejercicio 8

--- bucles/ejercicio8.dart
+++ bucles/ejercicio8.dart
@@ -1,12 +1,22 @@
 void main(List<String> args) {
-  int dividendo = 25;
+  int dividendo = -18;
   int divisor = 5;
-
   int respuesta = 0;
-  while (dividendo >= divisor) {
-    dividendo = dividendo - divisor;
-    respuesta++;
-  }
 
+  if (dividendo * divisor > 0) {
+    // los dos signos son iguales
+    dividendo = dividendo.abs();
+    divisor = divisor.abs();
+    while (dividendo >= divisor) {
+      dividendo = dividendo - divisor;
+      respuesta++;
+    }
+  } else {
+    // si los dos signos no son iguales, hago lo contrario
+    while (dividendo <= -divisor) {
+      dividendo = dividendo + divisor;
+      respuesta--;
+    }
+  }
   print("La respuesta es ${respuesta} con residuo ${dividendo}");
 }
