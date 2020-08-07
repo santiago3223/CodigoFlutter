339a0398e34cd28ee03cd6e3cc9ca210524bd545
Santiago <gerencia@evolutionsoluciones.com>
2020-08-06 21:16:39 -0500

    jklsdf

--- Clases/punto.dart
+++ Clases/punto.dart
@@ -9,6 +9,10 @@ class Punto {
     this.y = y;
   }
 
+  double operator -(Punto p) {
+    return pow(pow(this.x - p.x, 2) + pow(this.y - p.y, 2), 0.5);
+  }
+
   double distancia(Punto p) {
     return pow(pow(this.x - p.x, 2) + pow(this.y - p.y, 2), 0.5);
   }
--- Clases/repartos.dart
+++ Clases/repartos.dart
@@ -10,6 +10,9 @@ void main() {
   tiendas.add(new Tienda("Tu Tiendita", new Punto(-2, 3)));
   tiendas.add(new Tienda("Nuestra Tiendita", new Punto(1, 1)));
   tiendas.add(new Tienda("Libreria 2x2", new Punto(-1, -4)));
+
+  tiendas.sort((a, b) =>
+      (a.ubicacion.distancia(miCasa)).compareTo(b.ubicacion.distancia(miCasa)));
+
   print(tiendas);
-  print(tiendas[0].ubicacion.distancia(tiendas[1].ubicacion));
 }
