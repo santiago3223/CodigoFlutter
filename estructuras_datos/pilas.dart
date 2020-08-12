bd3905c47b5b3623ab0accaa8037ff9ab545cc95
Santiago <gerencia@evolutionsoluciones.com>
2020-08-11 19:52:22 -0500

    pilas

--- estructuras_datos/pilas.dart
+++ estructuras_datos/pilas.dart
@@ -1,35 +1,41 @@
 import 'dart:math';
 
 main(List<String> args) {
-  String expresion = "(a+[(b*5-[(a(7)+(35*5)]))])";
+  String expresion = "(a+[(b*5-[(a(7)+(35*5))])])";
   print(estaBalanceado(expresion));
 }
 
 bool estaBalanceado(String expresion) {
-  Pila p = new Pila();
+  Pila<String> p = new Pila();
   for (int i = 0; i < expresion.length; i++) {
-    if (expresion[i] == "(") p.Push(1);
-    if (expresion[i] == ")") p.Pop();
+    if (expresion[i] == "(") p.Push("(");
+    if (expresion[i] == ")") {
+      if (p.Pop() != "(") return false;
+    }
+    if (expresion[i] == "[") p.Push("[");
+    if (expresion[i] == "]") {
+      if (p.Pop() != "[") return false;
+    }
   }
   return p.estaVacio();
 }
 
-class Nodo {
-  int valor;
+class Nodo<E> {
+  E valor;
   Nodo anterior;
   Nodo({this.valor, this.anterior});
 }
 
-class Pila {
-  Nodo superior = null;
+class Pila<T> {
+  Nodo<T> superior = null;
 
-  void Push(int n) {
-    Nodo nodo = new Nodo(valor: n, anterior: superior);
+  void Push(T n) {
+    Nodo<T> nodo = new Nodo(valor: n, anterior: superior);
     superior = nodo;
   }
 
-  int Pop() {
-    int valor = superior.valor;
+  T Pop() {
+    T valor = superior.valor;
     superior = superior.anterior;
     return valor;
   }
