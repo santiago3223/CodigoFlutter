a61032b16885431b15d3d2a03541d1e79e2292a3
Santiago <gerencia@evolutionsoluciones.com>
2020-08-12 21:08:22 -0500

    imprimir grafos

--- estructuras_datos/grafos.dart
+++ estructuras_datos/grafos.dart
@@ -5,11 +5,27 @@ void main(List<String> args) {
   g.agregarArista(2, 5);
   g.agregarArista(7, 2);
   g.agregarArista(2, 1);
+  g.agregarArista(5, 3);
+  g.agregarArista(3, 1);
+  g.agregarArista(5, 7);
   g.imprimir();
+  print(g.cantidadVecinos(5));
 }
 
 class Grafo {
   List<Arista> aristas = new List();
+
+  void imprimirVecinos(int n) {}
+
+  int cantidadVecinos(int n) {
+    int cantidad = 0;
+    for (int i = 0; i < aristas.length; i++) {
+      if (aristas[i].v1.valor == n || aristas[i].v2.valor == n) {
+        cantidad++;
+      }
+    }
+    return cantidad;
+  }
 
   Vertice buscarOCrearVertice(int num1) {
     for (int i = 0; i < aristas.length; i++) {
@@ -35,7 +51,7 @@ class Grafo {
     File file = new File('grafo');
     String str = "graph {";
     for (int i = 0; i < aristas.length; i++) {
-      str += '${aristas[i].v1.valor} -- ${aristas[i].v2.valor}\n';
+      str += '${aristas[i].v1.valor} -- ${aristas[i].v2.valor};\n';
     }
     str += "}";
     file.writeAsString(str);
