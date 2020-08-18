720670aab714fa58643ae016ed3f4f752a8b7cf5
Santiago <gerencia@evolutionsoluciones.com>
2020-08-17 20:04:18 -0500

    interfaz 1

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void contactanos(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("contactanos"),
+          content: Text("contacto codigo"),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,26 +24,32 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepPurple,
         ),
         body: Center(
-          child: Column(
-            children: [
-              Text(
-                "Hola Santiago",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amber,
-                ),
+          child: Column(children: [
+            Text(
+              "Hola Santiago",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
               ),
-              Text(
-                "Vamos a programar",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black26,
-                ),
-              )
-            ],
-          ),
+            ),
+            Text(
+              "A programar",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            Image.network(
+              'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+              height: 200,
+            ),
+            RaisedButton(
+              child: Text("presioname"),
+              onPressed: () => contactanos(context),
+            )
+          ]),
         ),
       ),
     );
