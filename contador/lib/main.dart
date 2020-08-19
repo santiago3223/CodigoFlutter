81359a8928468401fc861b65caefb2a953cbc69a
Santiago <gerencia@evolutionsoluciones.com>
2020-08-18 19:06:00 -0500

    flutter

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -1,108 +1,43 @@
-import 'package:contador/texto.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: WidgetsPrueba());
+    return MaterialApp(home: PaginaAutos(nombre: "AppAutos"));
   }
 }
 
-class WidgetsPrueba extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          height: 300,
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Row(
-              children: [
-                Card(
-                  elevation: 5,
-                  color: Colors.red,
-                  shadowColor: Colors.blue,
-                  child: Container(
-                    width: 100,
-                    child: Text("Codigo"),
-                  ),
-                ),
-                Text("hola codigo"),
-                Container(
-                  width: 100,
-                  color: Colors.black,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+class PaginaAutos extends StatelessWidget {
+  String nombre;
 
-class MyPage extends StatelessWidget {
-  void contactanos(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("contactanos"),
-          content: Text("contacto codigo"),
-        );
-      },
-    );
-  }
+  PaginaAutos({this.nombre});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hola CodiGo"),
-        backgroundColor: Colors.deepPurple,
+        title: Text(nombre),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Hola Santiago",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amber,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "A programar",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-            Image.network(
-              'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
-              height: 200,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                child: Text("presioname"),
-                onPressed: () => contactanos(context),
-              ),
-            )
-          ]),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text("VW"),
+              Text("Gol"),
+              Image.network(
+                  "https://acs2.blob.core.windows.net/imgcatalogo/m/VA_674e942d8f56473bafccf1184a89712c.jpg"),
+              Text("VW"),
+              Text("Gol"),
+              Image.network(
+                  "https://acs2.blob.core.windows.net/imgcatalogo/m/VA_674e942d8f56473bafccf1184a89712c.jpg"),
+              Text("VW"),
+              Text("Gol"),
+              Image.network(
+                  "https://acs2.blob.core.windows.net/imgcatalogo/m/VA_674e942d8f56473bafccf1184a89712c.jpg"),
+            ],
+          ),
         ),
       ),
     );
