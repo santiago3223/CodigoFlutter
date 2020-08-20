d4d633038000b24bb824453dfce07a0bee93e4d0
Santiago <gerencia@evolutionsoluciones.com>
2020-08-19 21:57:58 -0500

    perfil2

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -53,16 +54,67 @@ class PaginaPerfil2 extends StatelessWidget {
                       fontSize: 30,
                       fontStyle: FontStyle.italic),
                 ),
-                Container(
-                  height: 230,
-                  margin: EdgeInsets.only(left: 30, right: 30, top: 30),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: Image.network(
-                        "https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/food%2Fmeal.jpg?alt=media",
-                        fit: BoxFit.cover),
-                  ),
+                Stack(
+                  children: [
+                    Container(
+                      height: 230,
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 30),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.network(
+                            "https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/food%2Fmeal.jpg?alt=media",
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Text("3.7km away")),
+                      ),
+                    )
+                  ],
                 ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Sasha - 22",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      "San Diego California, USA",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(FontAwesomeIcons.instagram, color: Colors.grey),
+                    SizedBox(width: 25),
+                    Icon(FontAwesomeIcons.facebookF, color: Colors.grey),
+                    SizedBox(width: 25),
+                    Icon(FontAwesomeIcons.twitter, color: Colors.grey)
+                  ],
+                )
               ],
             ),
           )
