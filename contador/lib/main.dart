d610507000cdd9b008a3368469e5295a79438426
Santiago <gerencia@evolutionsoluciones.com>
2020-08-19 20:53:13 -0500

    perfil2

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -13,7 +13,22 @@ class PaginaPerfil2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("perfil2"),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50)),
+              gradient: LinearGradient(colors: [
+                Colors.pink.shade400,
+                Colors.orange.shade300,
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            height: 350,
+          )
+        ],
+      ),
     );
   }
 }
