f9591059b0a04ea30d83c56a80ac7738a5338047
Santiago <gerencia@evolutionsoluciones.com>
2020-08-26 20:33:29 -0500

    conversor de unidades

--- Flutter/conversor_unidades/lib/main.dart
+++ Flutter/conversor_unidades/lib/main.dart
@@ -39,8 +39,25 @@ class _BodyConversorUnidadesState extends State<BodyConversorUnidades> {
     'onzas'
   ];
 
+  List _formulas = [
+    [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  ];
+
   void convertir(double valor, String uInicial, String uConvertida) {
-    resultado = "";
+    double res = valor *
+        _formulas[unidades.indexOf(uInicial)][unidades.indexOf(uConvertida)];
+    if (res == 0) {
+      resultado = "No se puede convertir esas unidades";
+    } else {
+      resultado = "$valor $uInicial equivale a $res $uConvertida";
+    }
   }
 
   @override
