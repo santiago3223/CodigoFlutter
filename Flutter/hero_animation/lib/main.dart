cba58e678279dfb6daa0d98a2ee8257fd50d9b04
Santiago <gerencia@evolutionsoluciones.com>
2020-09-03 21:01:40 -0500

    bottom nab v1

--- Flutter/hero_animation/lib/main.dart
+++ Flutter/hero_animation/lib/main.dart
@@ -20,29 +20,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Page1 extends StatelessWidget {
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  int _currentIndex = 0;
+  List _listPages = List();
+  Widget _currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    _listPages.add(Home());
+    _listPages.add(Birthday());
+    _listPages.add(Calendar());
+    _listPages.add(Alarm());
+    _currentIndex = 0;
+    _currentPage = _listPages[0];
+  }
+
+  void cambiarPagina(int i) {
+    setState(() {
+      _currentIndex = i;
+      _currentPage = _listPages[i];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hero animation"),
       ),
-      body: GestureDetector(
-        child: Hero(
-          tag: "etiquetaIcono",
-          child: Icon(
-            Icons.ac_unit,
-            color: Colors.green,
-            size: 40,
-          ),
-        ),
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (c) => Page2()));
-        },
-      ),
+      body: _currentPage,
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        currentIndex: 0,
+        currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
             title: Text("Home"),
@@ -61,7 +76,54 @@ class Page1 extends StatelessWidget {
             icon: Icon(Icons.alarm),
           ),
         ],
+        onTap: (index) => cambiarPagina(index),
       ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Hero(
+        tag: "etiquetaIcono",
+        child: Icon(
+          Icons.home,
+          color: Colors.green,
+          size: 40,
+        ),
+      ),
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (c) => Page2()));
+      },
+    );
+  }
+}
+
+class Birthday extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Icon(Icons.cake),
+    );
+  }
+}
+
+class Calendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Icon(Icons.calendar_today),
+    );
+  }
+}
+
+class Alarm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Icon(Icons.alarm),
     );
   }
 }
