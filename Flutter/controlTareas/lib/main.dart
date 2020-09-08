21e15f42847ac0211e6800265b7faadf87d2096f
Santiago <gerencia@evolutionsoluciones.com>
2020-09-08 18:25:24 -0500

    tareas

---
+++ Flutter/controlTareas/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ControlTareas(),
+    );
+  }
+}
+
+class ControlTareas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Control de Tareas"),
+          bottom: TabBar(tabs: [
+            Tab(
+              text: "To Do",
+            ),
+            Tab(
+              text: "Doing",
+            ),
+            Tab(
+              text: "Done",
+            )
+          ]),
+        ),
+        body: TabBarView(children: [Text("todo"), Text("doing"), Text("done")]),
+      ),
+    );
+  }
+}
