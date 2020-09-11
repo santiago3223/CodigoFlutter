b159c3cb1eb15c04935cf8cf06b9fc017dd8077e
Santiago <gerencia@evolutionsoluciones.com>
2020-09-10 21:59:45 -0500

    peliculas

--- Flutter/peliculasweb/lib/util/http_helper.dart
+++ Flutter/peliculasweb/lib/util/http_helper.dart
@@ -6,12 +6,26 @@ import 'package:peliculasweb/models/movie.dart';
 class HttpHelper {
   String urlkey = "api_key=aaf0f6007bb77eb949b1c0b23578d774";
   String urlBase = "https://api.themoviedb.org/3/movie";
+  String urlSearchBase = 'https://api.themoviedb.org/3/search/movie?';
   String urlUpcoming = "/upcoming?";
   String urlLanguage = "&language=en-US";
 
   Future<List> getUpcoming() async {
     String upcoming = urlBase + urlUpcoming + urlkey + urlLanguage;
     http.Response result = await http.get(upcoming);
+    if (result.statusCode == HttpStatus.ok) {
+      dynamic jsonResponse = json.decode(result.body);
+      dynamic moviesMap = jsonResponse["results"];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      return null;
+    }
+  }
+
+  Future<List> findMovies(String title) async {
+    String query = urlSearchBase + urlkey + "&query=" + title;
+    http.Response result = await http.get(query);
     if (result.statusCode == HttpStatus.ok) {
       dynamic jsonResponse = json.decode(result.body);
       dynamic moviesMap = jsonResponse["results"];
