a97683fd085d25a9efe94f2b6d9885cfbd095971
Santiago <gerencia@evolutionsoluciones.com>
2020-09-11 20:49:21 -0500

    pokedex

--- Flutter/pokedex/lib/route/pokemon_detail.dart
+++ Flutter/pokedex/lib/route/pokemon_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/models/pokemon.dart';
 import 'package:pokedex/util/http_helper.dart';
 
 class PokemonDetail extends StatefulWidget {
@@ -28,21 +29,66 @@ class _PokemonDetailState extends State<PokemonDetail> {
       appBar: AppBar(
         title: Text("Pokemon seleccionado"),
       ),
-      body: Column(
-        children: [
-          Hero(
-              tag: id.toString(),
-              child: Image.network(imageUrl + id.toString() + ".png")),
-          FutureBuilder(
-              future: helper.getPokemon(id),
-              builder: (c, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data.name);
-                } else if (snapshot.hasError) {}
-                return Text("Cargando");
-              })
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Hero(
+                tag: id.toString(),
+                child: Image.network(
+                  imageUrl + id.toString() + ".png",
+                  height: 200,
+                )),
+            FutureBuilder(
+                future: helper.getPokemon(id),
+                builder: (c, snapshot) {
+                  if (snapshot.hasData) {
+                    Pokemon pokemon = snapshot.data;
+                    return buildPokemonDetail(pokemon);
+                  } else if (snapshot.hasError) {
+                    return Text("Error");
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                })
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget buildPokemonDetail(Pokemon pokemon) {
+    return Column(
+      children: [
+        Text(
+          pokemon.name,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Peso: ${pokemon.weight}"),
+            Text("Tamaño: ${pokemon.height}")
+          ],
+        ),
+        Container(
+          height: 100,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Image.network(pokemon.sprites.frontDefault),
+              Image.network(pokemon.sprites.backDefault),
+              Image.network(pokemon.sprites.frontShiny),
+              Image.network(pokemon.sprites.backShiny)
+            ],
+          ),
+        )
+      ],
     );
   }
 }
