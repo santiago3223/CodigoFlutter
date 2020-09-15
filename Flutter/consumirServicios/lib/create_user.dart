91cef64f516b51e83774c2b3d71246637a5d0f66
Santiago <gerencia@evolutionsoluciones.com>
2020-09-14 21:39:36 -0500

    vista 1

--- Flutter/consumirServicios/lib/create_user.dart
+++ Flutter/consumirServicios/lib/create_user.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class CreateUser extends StatefulWidget {
   @override
@@ -12,12 +13,19 @@ class _CreateUserState extends State<CreateUser> {
   TextEditingController usuario = TextEditingController();
   TextEditingController trabajo = TextEditingController();
   GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey();
+  bool estaCargando = false;
 
   Future<String> crearUsuario() async {
+    setState(() {
+      estaCargando = true;
+    });
     var response = await http.post("https://reqres.in/api/users",
         body: {"name": usuario.text, "job": trabajo.text});
     print(response.statusCode);
     print(response.body.toString());
+    setState(() {
+      estaCargando = false;
+    });
     return response.body.toString();
   }
 
@@ -26,47 +34,50 @@ class _CreateUserState extends State<CreateUser> {
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: usuario,
-              decoration: InputDecoration(
-                  hintText: "Usuario",
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: trabajo,
-              decoration: InputDecoration(
-                  hintText: "Trabajo",
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-              child: Text("Crear"),
-              onPressed: () async {
-                String usr = await crearUsuario();
-                scaffoldKey.currentState.showSnackBar(
-                  SnackBar(
-                    content: Text(usr),
-                  ),
-                );
-              },
-            )
-          ],
+      body: ModalProgressHUD(
+        inAsyncCall: estaCargando,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: usuario,
+                decoration: InputDecoration(
+                    hintText: "Usuario",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: trabajo,
+                decoration: InputDecoration(
+                    hintText: "Trabajo",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RaisedButton(
+                child: Text("Crear"),
+                onPressed: () async {
+                  String usr = await crearUsuario();
+                  scaffoldKey.currentState.showSnackBar(
+                    SnackBar(
+                      content: Text(usr),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
