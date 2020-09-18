54ed413901c347ad0f4ca2c094cf4ca4bbf52f54
Santiago <gerencia@evolutionsoluciones.com>
2020-09-17 19:24:46 -0500

    clima

--- Flutter/weather_app/lib/views/weather_info.dart
+++ Flutter/weather_app/lib/views/weather_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/models/forecast.dart';
+import 'package:weather_app/models/weather.dart';
 
 class WeatherInfo extends StatelessWidget {
   const WeatherInfo({
@@ -8,6 +9,11 @@ class WeatherInfo extends StatelessWidget {
   }) : super(key: key);
 
   final Forecast forecast;
+
+  String formatTemp(double temp) {
+    temp -= 273.15;
+    return temp.round().toString();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +27,14 @@ class WeatherInfo extends StatelessWidget {
         Column(
           children: [
             Text(
-              "${forecast.current.temp}°",
+              "${formatTemp(forecast.current.temp)}°",
               style: TextStyle(
                   fontSize: 50,
                   color: Colors.white,
                   fontWeight: FontWeight.w300),
             ),
             Text(
-              "Sensación termica ${forecast.current.feelLikeTemp}°",
+              "Sensación termica ${formatTemp(forecast.current.feelLikeTemp)}°",
               style: TextStyle(
                   fontSize: 18,
                   color: Colors.white,
@@ -36,12 +42,40 @@ class WeatherInfo extends StatelessWidget {
             ),
           ],
         ),
-        Image.asset(
-          "assets/images/021-cloud.png",
-          height: 100,
-          width: 100,
-        )
+        buildWeatherImage(forecast.current.condition)
       ],
+    );
+  }
+
+  Image buildWeatherImage(WeatherCondition condition) {
+    String imageString = "assets/images/021-cloud.png";
+
+    switch (condition) {
+      case WeatherCondition.clear:
+      case WeatherCondition.lightCloud:
+        imageString = "assets/images/021-sun.png";
+        break;
+      case WeatherCondition.fog:
+      case WeatherCondition.atmosphere:
+      case WeatherCondition.rain:
+      case WeatherCondition.drizzle:
+      case WeatherCondition.mist:
+      case WeatherCondition.heavyCloud:
+        imageString = "assets/images/021-cloudy.png";
+        break;
+      case WeatherCondition.snow:
+        imageString = "assets/images/021-snowing-1.png";
+        break;
+      case WeatherCondition.thunderstorm:
+        imageString = "assets/images/021-rain-1.png";
+        break;
+      default:
+        imageString = "assets/images/021-cloudy.png";
+    }
+    return Image.asset(
+      imageString,
+      height: 100,
+      width: 100,
     );
   }
 }
