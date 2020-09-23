b7036de292d8a7af0a7cfbfde0bb6c5d7380bd35
Santiago <gerencia@evolutionsoluciones.com>
2020-09-22 21:41:11 -0500

    noticiapi

--- Flutter/noticiapi/lib/main.dart
+++ Flutter/noticiapi/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:noticiapi/utils/http_helper.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'models/noticia.dart';
 
 void main() {
@@ -36,6 +36,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   DateFormat fomatoFecha = DateFormat("dd/MM/yy");
   DateTime fechaSeleccionada = DateTime.now();
+  TextEditingController controller = TextEditingController(text: "Peru");
+  String textoBusqueda = "Peru";
 
   @override
   Widget build(BuildContext context) {
@@ -45,26 +47,43 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
-          FlatButton(
-              onPressed: () {
-                DatePicker.showDatePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime(2018, 1, 1),
-                    maxTime: DateTime.now(), onChanged: (date) {
-                  print('change $date');
-                }, onConfirm: (date) {
-                  setState(() {
-                    fechaSeleccionada = date;
-                  });
-                }, currentTime: DateTime.now(), locale: LocaleType.es);
-              },
-              child: Text(
-                fomatoFecha.format(fechaSeleccionada),
-                style: TextStyle(color: Colors.blue),
-              )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  controller: controller,
+                  onSubmitted: (value) {
+                    setState(() {
+                      textoBusqueda = value;
+                    });
+                  },
+                )),
+                FlatButton(
+                    onPressed: () {
+                      DatePicker.showDatePicker(context,
+                          showTitleActions: true,
+                          minTime: DateTime(2018, 1, 1),
+                          maxTime: DateTime.now(), onChanged: (date) {
+                        print('change $date');
+                      }, onConfirm: (date) {
+                        setState(() {
+                          fechaSeleccionada = date;
+                        });
+                      }, currentTime: DateTime.now(), locale: LocaleType.es);
+                    },
+                    child: Text(
+                      fomatoFecha.format(fechaSeleccionada),
+                      style: TextStyle(color: Colors.blue),
+                    )),
+              ],
+            ),
+          ),
           Expanded(
             child: FutureBuilder(
-              future: HttpHelper().fetchNoticias(fechaSeleccionada),
+              future:
+                  HttpHelper().fetchNoticias(fechaSeleccionada, textoBusqueda),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   List<Noticia> noticias = snapshot.data;
@@ -73,52 +92,61 @@ class _MyHomePageState extends State<MyHomePage> {
 
                   return ListView.builder(
                     itemCount: noticias.length,
-                    itemBuilder: (context, index) => Card(
-                      child: Row(
-                        children: [
-                          Image.network(
-                            noticias[index].urlToImage.toString(),
-                            height: 150,
-                            width: 150,
-                            fit: BoxFit.cover,
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  Text(
-                                    noticias[index].title.toString(),
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    noticias[index].source.name.toString() +
-                                        " - " +
-                                        fomatoFecha
-                                            .format(noticias[index].publishedAt)
-                                            .toString(),
-                                    textAlign: TextAlign.end,
-                                    style: TextStyle(fontSize: 12),
-                                  ),
-                                  Text(
-                                    noticias[index].description.toString(),
-                                    textAlign: TextAlign.justify,
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(noticias[index].author.toString(),
+                    itemBuilder: (context, index) => GestureDetector(
+                      onTap: () async {
+                        if (await canLaunch(noticias[index].url)) {
+                          await launch(noticias[index].url);
+                        }
+                      },
+                      child: Card(
+                        child: Row(
+                          children: [
+                            Image.network(
+                              noticias[index].urlToImage.toString(),
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.cover,
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Text(
+                                      noticias[index].title.toString(),
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      noticias[index].source.name.toString() +
+                                          " - " +
+                                          fomatoFecha
+                                              .format(
+                                                  noticias[index].publishedAt)
+                                              .toString(),
                                       textAlign: TextAlign.end,
-                                      style: TextStyle(fontSize: 12)),
-                                ],
+                                      style: TextStyle(fontSize: 12),
+                                    ),
+                                    Text(
+                                      noticias[index].description.toString(),
+                                      textAlign: TextAlign.justify,
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(noticias[index].author.toString(),
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(fontSize: 12)),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   );
--- Flutter/noticiapi/lib/utils/http_helper.dart
+++ Flutter/noticiapi/lib/utils/http_helper.dart
@@ -8,10 +8,10 @@ class HttpHelper {
   String baseUrl = "http://newsapi.org/v2/";
   String apiKey = "64ba406e93ad4cf09919bbfd94942ed6";
 
-  Future<List<Noticia>> fetchNoticias(DateTime fecha) async {
+  Future<List<Noticia>> fetchNoticias(DateTime fecha, String busqueda) async {
     DateFormat formatoFecha = DateFormat("yyyy-MM-dd");
     var response = await http.get(baseUrl +
-        "everything?from=${formatoFecha.format(fecha)}&to=${formatoFecha.format(fecha)}&qInTitle=peru&apiKey=" +
+        "everything?from=${formatoFecha.format(fecha)}&to=${formatoFecha.format(fecha)}&qInTitle=$busqueda&apiKey=" +
         apiKey);
     List noticiasJson = jsonDecode(response.body)["articles"];
     List noticias = noticiasJson.map((e) => Noticia.fromJson(e)).toList();
