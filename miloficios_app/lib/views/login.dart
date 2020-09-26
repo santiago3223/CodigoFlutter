5f4eb9acf69ff461131b8d808892323cf7541711
Santiago <gerencia@evolutionsoluciones.com>
2020-09-26 09:44:08 -0500

    login

--- miloficios_app/lib/views/login.dart
+++ miloficios_app/lib/views/login.dart
@@ -41,11 +41,62 @@ class _LoginState extends State<Login> {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      TextField(),
-                      TextField(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Mil Oficios",
+                          style: Theme.of(context).textTheme.headline3,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.account_circle),
+                          hintText: "Usuario",
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide:
+                                BorderSide(color: Colors.yellow, width: 2),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          hintText: "Password",
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide:
+                                BorderSide(color: Colors.yellow, width: 2),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
                       RaisedButton(
+                        color: Colors.blue,
                         onPressed: () {},
-                        child: Text("Iniciar Sesión"),
+                        child: Text(
+                          "Iniciar Sesión",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
                       )
                     ],
                   ),
