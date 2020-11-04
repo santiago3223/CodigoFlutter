0fcf3e22188baf700a5dd359b9f0426ce8617afd
Santiago <gerencia@evolutionsoluciones.com>
2020-11-03 21:39:29 -0500

    reconocimiento de imagenes

---
+++ pinguino/android/app/src/main/kotlin/com/example/pinguino/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pinguino
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pinguino/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flare_flutter/flare_controller.dart';
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_dart/math/mat2d.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Rockin\' Penguin',
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new MyHomePage(title: 'Rockin\' Penguin'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with FlareController {
+  double _rockAmount = 0.5;
+  double _speed = 1.0;
+  double _rockTime = 0.0;
+  bool _isPaused = false;
+
+  ActorAnimation _rock;
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    _rock = artboard.getAnimation("music_walk");
+  }
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {}
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    _rockTime += elapsed * _speed;
+    _rock.apply(_rockTime % _rock.duration, artboard, _rockAmount);
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: new AppBar(title: new Text(widget.title)),
+      body: new Stack(
+        children: [
+          Positioned.fill(
+              child: FlareActor(
+            "assets/Penguin.flr",
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+            animation: "walk",
+          )),
+        ],
+      ),
+    );
+  }
+}
